92a244f28ed2dc398d0a46117882300f46e95dc0
rafal66227 <rafal66227@mail.com>
2018-01-19 17:02:34 +0100

    sd

--- Przyklad1/Przyklad1/Program.cs
+++ Przyklad1/Przyklad1/Program.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Przyklad1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            //Kuciap to pedaaaał
-        }
-    }
-}
+﻿sss
