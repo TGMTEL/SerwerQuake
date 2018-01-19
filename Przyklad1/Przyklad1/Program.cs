f58a6a6fb413c18b5f748199961ed557c15f481f
rafal66227 <rafal66227@mail.com>
2018-01-19 17:09:51 +0100

    ssd

--- Przyklad1/Przyklad1/Program.cs
+++ Przyklad1/Przyklad1/Program.cs
@@ -1,1 +1,16 @@
-﻿sss
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przyklad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
